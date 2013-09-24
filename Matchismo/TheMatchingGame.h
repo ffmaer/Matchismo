3eb70ba314c53422d088b48b6637355ee9bbe9bc
ffmaer <ffmaer@gmail.com>
2013-09-24 15:37:56 -0400

    add project

---
+++ Matchismo/TheMatchingGame.h
@@ -0,0 +1,19 @@
+//
+//  TheMatchingGame.h
+//  Matchismo
+//
+//  Created by tcz on 8/18/13.
+//  Copyright (c) 2013 Maer Studios Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+@interface TheMatchingGame : NSObject
+
+
+-(id)initWithCardCount: (NSUInteger) count usingDeck:(Deck *)deck;
+-(Card* )cardAtIndex: (NSUInteger) index;
+-(NSString *) flipCardAtIndex:(NSUInteger) index;
+@property (nonatomic) NSUInteger mode;
+@property (readonly,nonatomic) NSInteger score;
+@end
